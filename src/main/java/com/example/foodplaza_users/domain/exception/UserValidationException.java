054ab8038b0bb0a2a68976950fd0b07621084794
Yusleidy20 <yusgamboa16@gmail.com>
2024-12-09 08:32:10 -0500

    hu-8-

---
+++ src/main/java/com/example/foodplaza_users/domain/exception/UserValidationException.java
@@ -0,0 +1,4 @@
+package com.example.foodplaza_users.domain.exception;
+
+public class UserValidationException {
+}
---
+++ src/main/java/com/example/foodplaza_users/infrastructure/configuration/segurity/jwt/JwtService.java
@@ -0,0 +1,4 @@
+package com.example.foodplaza_users.infrastructure.configuration.segurity.jwt;
+
+public class JwtService {
+}
